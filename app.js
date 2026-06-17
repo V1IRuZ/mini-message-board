d3889eb92f502fb656055c6da40065512578bb57
Esa <esa-pitkanen@hotmail.com>
2026-06-17 19:35:10 +0300

    feat: set up indexRouter

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const path = require("node:path");
 
 const app = express();
 
+const indexRouter = require("./routes/indexRouter.js");
+
 const assetsPath = path.join(__dirname, "public");
 app.use(express.static(assetsPath));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
 
 app.listen(3000, (err) => {
   if (err) {
