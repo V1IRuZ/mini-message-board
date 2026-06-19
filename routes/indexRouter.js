ff408c04f1a45401c0518527d0f66c82e6320bfe
Esa <esa-pitkanen@hotmail.com>
2026-06-19 13:25:33 +0300

    feat: add post method for adding new messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -23,4 +23,12 @@ indexRouter.get("/new", (req, res) => {
   res.render("form");
 });
 
+indexRouter.post("/new", (req, res) => {
+  const messageText = req.body.messageText;
+  const messageUser = req.body.messageUser;
+  
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect("/");
+});
+
 module.exports = indexRouter;
