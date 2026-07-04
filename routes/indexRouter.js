c15d86e565fe5e65024b702426db80ab8b5554b8
Esa <esa-pitkanen@hotmail.com>
2026-07-04 18:08:23 +0300

    refactor: use the controller's methods in routes

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,66 +1,12 @@
 const { Router } = require("express");
-const CustomNotFoundError = require("../errors/CustomNotFoundError.js");
-
+const controller = require("../controllers/controller.js");
 const indexRouter = Router();
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date(),
-    id: 1,
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date(),
-    id: 2,
-  },
-];
-
-indexRouter.get("/", (req, res) => {
-  res.render("index", { messages: messages });
-});
-
-indexRouter.get("/new", (req, res) => {
-  res.render("form");
-});
-
-indexRouter.get("/message", (req, res) => {
-  res.redirect("/");
-});
-
-indexRouter.get("/message/:messageId", (req, res) => {
-  const { messageId } = req.params;
-
-  const message = messages.find((msg) => msg.id === Number(messageId));
-
-  if (!message) {
-    throw new CustomNotFoundError("Message Not Found");
-  }
-
-  res.render("message", {
-    title: `Message`,
-    message: message,
-  });
-});
-
-indexRouter.post("/new", (req, res) => {
-  const messageText = req.body.messageText;
-  const messageUser = req.body.messageUser;
-
-  messages.push({
-    text: messageText,
-    user: messageUser,
-    added: new Date(),
-    id: messages.length + 1,
-  });
-
-  res.redirect("/");
-});
-
-indexRouter.get("/{*splat}", (req, res) => {
-  res.render("error");
-});
+indexRouter.get("/", controller.getMessages);
+indexRouter.get("/new", controller.getNewUser);
+indexRouter.get("/message", controller.getRedirectHome);
+indexRouter.get("/message/:messageId", controller.getMessage);
+indexRouter.post("/new", controller.postNewUser);
+indexRouter.get("/{*splat}", controller.getInvalidRoute);
 
 module.exports = indexRouter;
