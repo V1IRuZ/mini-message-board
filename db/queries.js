2e938f3b95232ae27152d18b7101a7d85b303276
Esa <esa-pitkanen@hotmail.com>
2026-07-04 16:24:29 +0300

    feat: add queries methods

---
+++ db/queries.js
@@ -0,0 +1,27 @@
+const pool = require("./pool");
+
+async function getAllmessages() {
+  const { rows } = await pool.query("SELECT * FROM messages");
+  return rows;
+}
+
+async function getMessage(id) {
+  const { rows } = await pool.query("SELECT * FROM messages WHERE id = $1", [
+    id,
+  ]);
+
+  return rows[0];
+}
+
+async function insertMessage(username, message) {
+  await pool.query("INSERT INTO messages (username, message) VALUES ($1, $2)", [
+    username,
+    message,
+  ]);
+}
+
+module.exports = {
+  getAllmessages,
+  getMessage,
+  insertMessage,
+};
